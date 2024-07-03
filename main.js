3e9be266afb785ff4fec3e2295f8a308687d28c3
techeo-p <estherola2018@gmail.com>
2024-07-03 23:45:36 +0100

    new website

---
+++ main.js
@@ -0,0 +1,64 @@
+function updateDateTime() {
+  let now = new Date();
+
+  let daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDayElement = document.getElementById("currentDay");
+  if (currentDayElement) {
+    currentDayElement.textContent = daysOfWeek[now.getUTCDay()];
+  }
+
+  let currentTimeUTCElement = document.getElementById("currentTimeUTC");
+  if (currentTimeUTCElement) {
+    currentTimeUTCElement.textContent = now
+      .toISOString()
+      .split("T")[1]
+      .slice(0, 8);
+  }
+}
+updateDateTime();
+setInterval(updateDateTime, 1000);
+
+// dropdown actions function
+document.addEventListener("DOMContentLoaded", function () {
+  var dropdownIcons = document.querySelectorAll(".dropdown-icon");
+
+  dropdownIcons.forEach(function (icon) {
+    icon.addEventListener("click", function () {
+      var parent = this.closest(".colProf");
+      parent.classList.toggle("expanded");
+    });
+  });
+});
+
+// navbar function
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
+    const offset = 100; // the offset
+
+    if (targetElement) {
+      const offsetPosition = targetElement.offsetTop - offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
+function toggleNavbar() {
+  var navbar = document.getElementById("navbar");
+  navbar.classList.toggle("active");
+}
