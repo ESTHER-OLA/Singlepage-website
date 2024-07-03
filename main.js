5743a6fbec5480f50d7a507dea2a456ec4b87637
techeo-p <estherola2018@gmail.com>
2024-07-04 00:04:23 +0100

    new website

--- main.js
+++ main.js
@@ -37,28 +37,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
-
-// navbar function
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    const targetId = this.getAttribute("href");
-    const targetElement = document.querySelector(targetId);
-    const offset = 100; // the offset
-
-    if (targetElement) {
-      const offsetPosition = targetElement.offsetTop - offset;
-
-      window.scrollTo({
-        top: offsetPosition,
-        behavior: "smooth",
-      });
-    }
-  });
-});
-
-function toggleNavbar() {
-  var navbar = document.getElementById("navbar");
-  navbar.classList.toggle("active");
-}
